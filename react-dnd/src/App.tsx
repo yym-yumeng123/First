545afbbda777e5822addc3595543105f11b0fcda
yym-yumeng123 <18026493873@163.com>
2025-01-06 17:46:25 +0800

    feat: 引入 useDrag和useDrop

--- react-dnd/src/App.tsx
+++ react-dnd/src/App.tsx
@@ -1,33 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useRef } from "react"
+import { useDrag, useDrop } from "react-dnd"
+import "./App.css"
+
+function Box() {
+  const ref = useRef<HTMLDivElement>(null)
+
+  const [, drag] = useDrag({
+    type: "box",
+    item: {
+      color: "blue",
+    },
+  })
+
+  drag(ref)
+
+  return <div ref={ref} className='box'></div>
+}
+
+function Container() {
+  const ref = useRef(null)
+
+  const [, drop] = useDrop(() => {
+    return {
+      accept: "box",
+      drop(item) {
+        console.log(item)
+      },
+    }
+  })
+  drop(ref)
+  return <div ref={ref} className='container'></div>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Container></Container>
+      <Box></Box>
     </>
   )
 }
