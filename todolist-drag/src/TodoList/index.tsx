face18ab389ff9a0fcba05a6623b252a5bf16c7a
yym-yumeng123 <18026493873@163.com>
2025-01-07 15:00:28 +0800

    feat: 安装依赖 tailwindcss postcss autoprefixer

---
+++ todolist-drag/src/TodoList/index.tsx
@@ -0,0 +1,12 @@
+import { FC } from "react"
+
+interface TodoListProps {
+}
+
+export const TodoList: FC<TodoListProps> = (props) => {
+  return (
+    <div className="">
+      TodoList
+    </div>
+  )
+}
---
+++ todolist-drag/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // 从哪里提取 className
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
