fc52bf409e05f098a90e5a7d1282938c5ae77484
yym-yumeng123 <18026493873@163.com>
2025-01-07 15:12:41 +0800

    feat: tailwind 没有内置的 class 在  extend 中配置

--- todolist-drag/src/TodoList/index.tsx
+++ todolist-drag/src/TodoList/index.tsx
@@ -1,12 +1,12 @@
 import { FC } from "react"
 
-interface TodoListProps {
-}
+interface TodoListProps {}
 
 export const TodoList: FC<TodoListProps> = (props) => {
   return (
-    <div className="">
-      TodoList
+    <div className='w-1000 h-600 m-auto mt-100 p-10 border-2 border-black flex justify-between items-start'>
+      <div className='flex-2 h-full mr-10 bg-blue-400 overflow-auto'></div>
+      <div className='flex-1 h-full bg-blue-400'></div>
     </div>
   )
 }
--- todolist-drag/tailwind.config.js
+++ todolist-drag/tailwind.config.js
@@ -3,7 +3,25 @@ export default {
   // 从哪里提取 className
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      // 内置的 className 没有, 需要加到这里面
+      width: {
+        1000: "1000px",
+      },
+      height: {
+        600: "600px",
+      },
+      margin: {
+        100: "100px",
+        10: "10px",
+      },
+      padding: {
+        10: "10px",
+      },
+      flex: {
+        2: 2
+      }
+    },
   },
   plugins: [],
 }
