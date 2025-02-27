edc0e2a304a035ed56866d7ee3e97faaacc01b47
yym-yumeng123 <18026493873@163.com>
2025-02-27 17:49:44 +0800

    feat: 给命令行添加命令

--- Node/包管理/脚手架/scaffold/index.js
+++ Node/包管理/脚手架/scaffold/index.js
@@ -2,8 +2,24 @@
 
 const { program } = require("commander")
 
+// 添加命令行参数, 在 --help 中显示
+program.option("-n, --name", "显示脚手架名称")
+program.option("-d, --dest <dest>", "a destination folder, default is current folder")
+program.option("-f, --framework <framework>", "a framework, default is vue3")
+
+program.on("--help", () => {
+  console.log("其他参数:")
+  console.log("  -n, --name 显示脚手架名称")
+  console.log("  -d, --dest 指定目标文件夹, 默认是当前文件夹")
+  console.log("  -f, --framework 指定框架, 默认是 vue3")
+})
+
 // 动态获取 package.json 中的 version 版本号
 program.version(require("./package.json").version)
 
 // 解析命令行参数
 program.parse(process.argv)
+
+// 获取命令行参数
+const options = program.opts()
+console.log(options)
