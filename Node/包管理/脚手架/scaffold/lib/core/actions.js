e5e7fd81b8d0275bea9e4e31c1ffd106f5c666df
yym-yumeng123 <18026493873@163.com>
2025-02-28 16:37:43 +0800

    fix: 路径问题导致无法安装

--- Node/包管理/脚手架/scaffold/lib/core/actions.js
+++ Node/包管理/脚手架/scaffold/lib/core/actions.js
@@ -1,24 +1,23 @@
 const { promisify } = require('util')
 const path = require("path")
 const { vueRepo, directRepo } = require("../config/repo-config")
-const { commandSpawn } = require("../utils/terminal")
+const { installDependencies } = require("../utils/terminal")
 
 const download = promisify(require("download-git-repo"))
 const createProject = async (project, others) => {
   console.log("创建项目", project, others)
-  // 1. 创建项目目录
+  // 1. 创建项目目录 返回项目路径
   const projectDir = path.join(process.cwd(), project)
   // 2. clone 项目
   await download(directRepo, projectDir, { clone: true })
+  console.log(projectDir, 'projectDir')
   // 3. 安装依赖
-  const isWindows = process.platform === "win32" ? "yarn.cmd" : "yarn";
-  await commandSpawn(isWindows, ["install"], { cwd: `./${projectDir}` });
+  await installDependencies(projectDir);
 
   // 4. npm run dev
-  await commandSpawn("npm", ["run", "dev"], { cwd: `./${projectDir}` });
 
   // 5. 打开浏览器
-  await commandSpawn("start", [`http://localhost:8080`], { cwd: `./${projectDir}` });
+  // await commandSpawn("start", [`http://localhost:8080`], { cwd: `./${projectDir}` });
 }
 
 module.exports = {
--- Node/包管理/脚手架/scaffold/lib/utils/terminal.js
+++ Node/包管理/脚手架/scaffold/lib/utils/terminal.js
@@ -6,22 +6,30 @@
 // spawn 方法可以创建一个子进程来执行命令
 const { spawn } = require("child_process")
 
-const commandSpawn = (...args) => {
-  // 子进程的输出和错误信息 返回
+function installDependencies(projectPath) {
   return new Promise((resolve, reject) => {
-    // 创建子进程
-    const childProcess = spawn(...args)
-    // 监听子进程的输出
-    childProcess.stdout.pipe(process.stdout)
-    childProcess.stderr.pipe(process.stderr)
-    // 监听子进程的退出
-    childProcess.on("close", () => {
-      resolve()
-    })
-  })
+    const isWindows = process.platform === "win32" ? "npm.cmd" : "npm";
+    const npm = spawn(isWindows, ['install'], {
+      cwd: projectPath, // 指定项目路径
+      stdio: 'inherit', // 继承父进程的输入输出
+    });
+
+    npm.on('close', (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`npm install 失败，退出码：${code}`));
+      }
+    });
+
+    npm.on('error', (err) => {
+      reject(new Error(`无法启动 npm 进程：${err.message}`));
+    });
+  });
 }
 
+
 module.exports = {
-  commandSpawn,
+  installDependencies,
 }
 
