05fc8ce532fbe3451345bb9fd864b4e625a55856
yym-yumeng123 <18026493873@163.com>
2025-03-06 10:22:48 +0800

    feat: 普通中间件

--- Node/Express/express_demo01/index.js
+++ Node/Express/express_demo01/index.js
@@ -3,7 +3,19 @@ const express = require("express")
 // 创建应用对象, 类似 http.createServer()
 const app = express()
 
-// 创建路由
+// 编写普通的中间件
+// use 方法注册中间件, 可以注册多个中间件
+app.use((req, res, next) => {
+  console.log("注册了第一个中间件")
+  next()
+})
+
+app.use((req, res, next) => {
+  console.log("注册了第二个中间件")
+  next()
+})
+
+// 编写路由
 app.get("/", (req, res) => {
   res.send("Hello World")
 })
