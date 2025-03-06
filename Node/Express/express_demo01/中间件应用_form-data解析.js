4673ebebca7668fc68a61fd90026e175950fdcb9
yym-yumeng123 <18026493873@163.com>
2025-03-06 11:16:01 +0800

    feat: 上传单个文件 multer.single()

--- Node/Express/express_demo01/中间件应用_form-data解析.js
+++ Node/Express/express_demo01/中间件应用_form-data解析.js
@@ -2,15 +2,30 @@ const express = require("express")
 const multer = require("multer")
 const app = express()
 
-const upload = multer()
-
-app.use(upload.any())
-
-app.post("/login", (req, res, next) => {
-  console.log(req.body, 'req.body')
-  res.send('登录成功')
+// 配置multer
+const upload = multer({
+  // 配置文件存储路径
+  dest: "./uploads/",
 })
 
+// app.use(upload.any())
+
+// app.post("/login", (req, res, next) => {
+//   console.log(req.body, 'req.body')
+//   res.send('登录成功')
+// })
+
+// 上传文件 单个文件 single('文件名key')
+app.post("/upload", upload.single("file"), (req, res, next) => {
+  console.log(req.file, "req.file")
+  res.send("上传成功")
+})
+
+// 上传文件 多个文件
+// app.post("/uploads", upload.array("files"), (req, res, next) => {
+//   console.log(req.body, 'req.body')
+//   res.send('上传成功')
+// })
 
 app.listen(3000, () => {
   console.log("form-data解析服务器启动成功")
