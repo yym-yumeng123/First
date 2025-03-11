61d534408a428b2d571ba546cbfab56d974b5cd4
yym-yumeng123 <18026493873@163.com>
2025-03-11 10:33:14 +0800

    feat: 项目分析

---
+++ express_project/README.md
@@ -0,0 +1,48 @@
+# Express Project
+
+### 目录解析
+
+- bin: 存放启动脚本
+- public: 存放静态文件
+- routes: 存放路由文件
+- app.js: 存放应用配置
+
+### 项目启动
+
+```bash
+npm start # nodemon
+```
+
+### MySQL
+
+```bash
+npm install mysql2
+```
+
+```sql
+# 插入
+INSERT INTO users (name, email, password) VALUES ('John', 'john@example.com', 'password123');
+
+# 更新
+UPDATE users SET email = 'john@example.com' WHERE id = 1;
+
+# 删除
+DELETE FROM users WHERE id = 1;
+
+# 查询
+SELECT * FROM users ORDER BY id DESC;
+```
+
+### 数据库连接
+
+```js
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '123456',
+  database: 'project01',
+});
+```
+
--- express_project/app.js
+++ express_project/app.js
@@ -1,17 +1,20 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var app = express();
+const app = express();
 
 app.use(logger('dev'));
+// 解析请求体
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+// 解析cookie
 app.use(cookieParser());
+// 静态文件
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
