a091d88a8b06e5fa691390a6b50c9bd40488451b
yym-yumeng123 <18026493873@163.com>
2025-03-11 15:03:14 +0800

    feat: 查询单个文章详情接口

--- express_project/routes/admins/articles.js
+++ express_project/routes/admins/articles.js
@@ -2,9 +2,9 @@ const express = require("express")
 const router = express.Router()
 const { Article } = require("../../models")
 
+// 获取文章列表
 router.get("/", async (req, res) => {
   try {
-    // 获取文章列表
     const articles = await Article.findAll({
       // 按id降序排序, 可能多个字段排序
       order: [["id", "DESC"]],
@@ -22,5 +22,31 @@ router.get("/", async (req, res) => {
     })
   }
 })
+
+// 查询文章详情
+router.get("/:id", async (req, res) => {
+  const { id } = req.params
+  try {
+    const article = await Article.findByPk(id)
+    if (!article) {
+      return res.status(404).json({
+        status: false,
+        message: "文章不存在",
+      })
+    }
+    res.json({
+      status: true,
+      message: "获取文章详情成功",
+      data: article,
+    })
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "获取文章详情失败",
+      errors: [error.message],
+    })
+  }
+})
+
 
 module.exports = router
