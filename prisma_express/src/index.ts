a182d5b70de4666214f2af6486febf16150c2b52
yym-yumeng123 <18026493873@163.com>
2025-03-24 17:06:10 +0800

    feat: 添加用户注册、文章创建、查看和发布功能

--- prisma_express/src/index.ts
+++ prisma_express/src/index.ts
@@ -1,18 +1,109 @@
-import express from 'express';
-import { PrismaClient } from '@prisma/client';
+import express from "express"
+import { PrismaClient } from "@prisma/client"
 
-const app = express();
-const prisma = new PrismaClient();
+const app = express()
+const prisma = new PrismaClient()
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('/users', async (req, res) => {
-  const users = await prisma.user.findMany();
-  res.json(users);
-});
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany()
+  res.json(users)
+})
 
-const PORT = process.env.PORT || 3000;
+// 注册用户
+app.post("/signup", async (req, res) => {
+  const { name, email } = req.body
+  const user = await prisma.user.create({
+    data: { name, email },
+  })
+  res.json(user)
+})
+
+// 新建文章
+app.post("/posts", async (req, res) => {
+  const { title, content, authorEmail } = req.body
+  const post = await prisma.post.create({
+    data: {
+      title,
+      content,
+      author: {
+        connect: {
+          email: authorEmail,
+        },
+      },
+    },
+  })
+  res.json(post)
+})
+
+// 给文章阅读量+1
+app.post("/posts/:id/views", async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.update({
+    where: { id: parseInt(id) },
+    data: {
+      viewCount: {
+        increment: 1,
+      },
+    },
+  })
+  res.json(post)
+})
+
+// 发布文章
+app.post("/publish/:id", async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.update({
+    where: { id: parseInt(id) },
+    data: {
+      published: true,
+    },
+  })
+  res.json(post)
+})
+
+// 查询某个用户草稿
+app.get("/user/:id/drafts", async (req, res) => {
+  const { id } = req.params
+  const drafts = await prisma.user
+    .findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    })
+    .posts({
+      where: {
+        published: false,
+      },
+    })
+  res.json(drafts)
+})
+
+// 查询单个文章
+app.get("/post/:id", async (req, res) => {
+  const { id } = req.params
+  const post = await prisma.post.findUnique({
+    where: { id: parseInt(id) },
+  })
+  res.json(post)
+})
+
+// 查询所有已发布文章
+app.get("/feed", async (req, res) => {
+  const { take, skip } = req.query
+  const posts = await prisma.post.findMany({
+    where: {
+      published: true,
+    },
+    skip: parseInt(skip as string),
+    take: parseInt(take as string),
+  })
+  res.json(posts)
+})
+
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-}); 
+  console.log(`Server is running on port ${PORT}`)
+})
